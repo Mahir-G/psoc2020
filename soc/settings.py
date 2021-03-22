d50b3cdec6be554c53f22347847cff20a82e6835
gurnameh <gurunameh.work@gmail.com>
2021-03-22 13:19:47 +0530

    replaced postgres with sqlite for development

--- soc/settings.py
+++ soc/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'soc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'psoc2020_two',
-        'USER': 'psoc2020_admin',
-        'PASSWORD': 'psoc2020_whattodo',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
